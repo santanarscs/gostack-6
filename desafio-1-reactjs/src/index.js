1fcbe85ffd5f962d745f047ad1803e07bd04b92b
santanarscs <raphaelstn@gmail.com>
2019-04-30 10:21:16 -0300

    finalizando config para desafio 1

--- desafio-1-reactjs/src/index.js
+++ desafio-1-reactjs/src/index.js
@@ -1,1 +1,9 @@
-alert('hellow');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.scss';
+class App extends Component {
+	render() {
+		return <h1>Hello mundo</h1>;
+	}
+}
+render(<App />, document.getElementById('app'));
--- desafio-1-reactjs/webpack.config.js
+++ desafio-1-reactjs/webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
 				use: {
 					loader: 'babel-loader'
 				}
+			},
+			{
+				test: /\.scss/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
 			}
 		]
 	}
