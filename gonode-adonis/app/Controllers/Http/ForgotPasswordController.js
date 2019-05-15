b3d1db114fabf71af2775b932b9521959d0264ae
santanarscs <raphaelstn@gmail.com>
2019-05-15 14:44:05 -0300

    resetando senha

--- gonode-adonis/app/Controllers/Http/ForgotPasswordController.js
+++ gonode-adonis/app/Controllers/Http/ForgotPasswordController.js
@@ -1,4 +1,5 @@
 'use strict'
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -29,6 +30,28 @@ class ForgotPasswordController {
       response
         .status(err.status)
         .send({ error: { message: 'Algo não deu certo' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+      if (tokenExpired) {
+        response.status(401).send({ error: { message: 'Token Expirado' } })
+      }
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      response
+        .status(err.status)
+        .send({ error: { message: 'Algo não deu certo ao resetar senha' } })
     }
   }
 }
