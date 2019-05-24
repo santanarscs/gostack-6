aa354741514c1a9148f1f1998b01a9a1c6428618
santanarscs <raphaelstn@gmail.com>
2019-05-24 14:27:24 -0300

    adicionei novo projeto react

---
+++ go-spotify/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/global";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
