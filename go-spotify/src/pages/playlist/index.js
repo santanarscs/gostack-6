2d56f91ce9d17c1aab6a7240228dc5e45cf1b42c
santanarscs <raphaelstn@gmail.com>
2019-05-25 19:26:09 -0300

    finalizando estilo spotfy

--- go-spotify/src/pages/playlist/index.js
+++ go-spotify/src/pages/playlist/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-import { Container, Header } from "./styles";
-
+import { Container, Header, SongList } from "./styles";
+import ClockIcon from "../../assets/images/clock.svg";
+import PlusIcon from "../../assets/images/plus.svg";
 export default function playlist() {
   return (
     <Container>
@@ -17,6 +18,73 @@ export default function playlist() {
           <button>PLAY</button>
         </div>
       </Header>
+      <SongList cellPadding={0} cellSpacing={0}>
+        <thead>
+          <th />
+          <th>Título</th>
+          <th>Artista</th>
+          <th>Album</th>
+          <th>
+            <img src={ClockIcon} alt="Duração" />
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={PlusIcon} alt="Adicionar" />
+            </td>
+            <td>Papercut</td>
+            <td>Linkin Park</td>
+            <td>Hybrid Theory</td>
+            <td>3:27</td>
+          </tr>
+          <tr>
+            <td>
+              <img src={PlusIcon} alt="Adicionar" />
+            </td>
+            <td>Papercut</td>
+            <td>Linkin Park</td>
+            <td>Hybrid Theory</td>
+            <td>3:27</td>
+          </tr>
+          <tr>
+            <td>
+              <img src={PlusIcon} alt="Adicionar" />
+            </td>
+            <td>Papercut</td>
+            <td>Linkin Park</td>
+            <td>Hybrid Theory</td>
+            <td>3:27</td>
+          </tr>
+          <tr>
+            <td>
+              <img src={PlusIcon} alt="Adicionar" />
+            </td>
+            <td>Papercut</td>
+            <td>Linkin Park</td>
+            <td>Hybrid Theory</td>
+            <td>3:27</td>
+          </tr>
+          <tr>
+            <td>
+              <img src={PlusIcon} alt="Adicionar" />
+            </td>
+            <td>Papercut</td>
+            <td>Linkin Park</td>
+            <td>Hybrid Theory</td>
+            <td>3:27</td>
+          </tr>
+          <tr>
+            <td>
+              <img src={PlusIcon} alt="Adicionar" />
+            </td>
+            <td>Papercut</td>
+            <td>Linkin Park</td>
+            <td>Hybrid Theory</td>
+            <td>3:27</td>
+          </tr>
+        </tbody>
+      </SongList>
     </Container>
   );
 }
