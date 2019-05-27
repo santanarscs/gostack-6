1d206084e35a6c411e3502c8d2f3f34c431501f5
santanarscs <raphaelstn@gmail.com>
2019-05-27 15:38:12 -0300

    configurando redux redux saga

--- go-spotify/src/App.js
+++ go-spotify/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import "./config/reactotron";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
 import "rc-slider/assets/index.css";
 import "./styles/styles.css";
 import Sidebar from "./components/Sidebar";
@@ -9,20 +11,23 @@ import Header from "./components/Header";
 import { Wrapper, Container, Content } from "./styles/components";
 
 import Routes from "./routes";
+import store from "./store";
 function App() {
   return (
-    <BrowserRouter>
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-            <Routes />
-          </Content>
-        </Container>
-        <Player />
-      </Wrapper>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+        </Wrapper>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
