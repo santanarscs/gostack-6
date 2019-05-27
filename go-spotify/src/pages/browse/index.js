d6c9ee4961c79a11c8df1bf3d2af25aac17b8189
santanarscs <raphaelstn@gmail.com>
2019-05-27 16:16:23 -0300

    mostrando paylist na home

--- go-spotify/src/pages/browse/index.js
+++ go-spotify/src/pages/browse/index.js
@@ -1,57 +1,51 @@
-import React from "react";
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Creators as PlaylistsActions } from "../../store/ducks/playlists";
 import { Container, Title, List, Playlist } from "./styles";
 
-export default function browse() {
-  return (
-    <Container>
-      <Title>Navegar</Title>
-      <List>
-        <Playlist to="/playlists/1">
-          <img
-            src="https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg"
-            alt="teste"
-          />
-          <strong>Rock dos bons</strong>
-          <p>
-            Relaxa enquanto você programa ouvindo apenas as melhores do rock
-            mundial
-          </p>
-        </Playlist>
-        <Playlist to="/playlists/1">
-          <img
-            src="https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg"
-            alt="teste"
-          />
-          <strong>Rock dos bons</strong>
-          <p>
-            Relaxa enquanto você programa ouvindo apenas as melhores do rock
-            mundial
-          </p>
-        </Playlist>
-        <Playlist to="/playlists/1">
-          <img
-            src="https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg"
-            alt="teste"
-          />
-          <strong>Rock dos bons</strong>
-          <p>
-            Relaxa enquanto você programa ouvindo apenas as melhores do rock
-            mundial
-          </p>
-        </Playlist>
-        <Playlist to="/playlists/1">
-          <img
-            src="https://blog.spoongraphics.co.uk/wp-content/uploads/2017/01/thumbnail-2.jpg"
-            alt="teste"
-          />
-          <strong>Rock dos bons</strong>
-          <p>
-            Relaxa enquanto você programa ouvindo apenas as melhores do rock
-            mundial
-          </p>
-        </Playlist>
-      </List>
-    </Container>
-  );
+class Browse extends Component {
+  static propTypes = {
+    getPlayListsRequest: PropTypes.func.isRequired,
+    playlists: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+          thumbnail: PropTypes.string,
+          description: PropTypes.string
+        })
+      )
+    }).isRequired
+  };
+  componentDidMount() {
+    this.props.getPlayListsRequest();
+  }
+  render() {
+    return (
+      <Container>
+        <Title>Navegar</Title>
+        <List>
+          {this.props.playlists.data.map(playlist => (
+            <Playlist key={playlist.id} to={`/playlists/${playlist.id}`}>
+              <img src={playlist.thumbnail} alt={playlist.title} />
+              <strong>{playlist.title}</strong>
+              <p>{playlist.description}</p>
+            </Playlist>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
+const mapStateToProps = state => ({
+  playlists: state.playlists
+});
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(PlaylistsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Browse);
